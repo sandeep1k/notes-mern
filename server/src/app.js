d5fcaecd5529898d79151981d1e7817bda025fd9
Sandeep Gaur <sandeepkrgraphics@gmail.com>
2024-03-14 12:08:08 +0530

    code push

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+// Importing necessary modules
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+
+// Importing route handlers
+import { errorHandler } from "./api/v1/middlewares/errorMiddleware.js";
+import noteRoutes from "./api/v1/notes/noteRoutes.js";
+import userRoutes from "./api/v1/users/userRoutes.js";
+
+// Initialize dotenv to use .env file variables
+dotenv.config();
+
+// Initialize express app
+const app = express();
+
+// Middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser()); // for parsing cookies
+app.use(cors()); // to enable CORS
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/notes", noteRoutes);
+
+// Error Handling Middleware - This should be after all the routes
+app.use(errorHandler);
+
+export default app;
